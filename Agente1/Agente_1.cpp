0911b93d9e07ec35e8e0b844e57d1187f4c02051
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-13 21:31:42 -0600

    program structure

---
+++ Agente1/Agente_1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<thread>
+#include<queue>
+
+
+
+
+typedef struct{
+
+}Percept;
+
+typedef struct{
+
+}Action;
+
+typedef struct{
+
+}Rules;
+/*
+typedef struct{
+    bool operator()(const Rules& a,const Rules& b)const{
+        return a.prioridad<b.prioridad;
+    }
+}Comparador
+
+*/
+using namespace std;
+
+
+
+
+void main(){
+    
+}
+
+
+
