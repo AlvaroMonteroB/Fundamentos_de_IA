15338d81b517706a88f277a33db27a5fcd72ed99
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-17 22:28:06 -0600

    Making objets

--- P1/main.py
+++ P1/main.py
@@ -1,5 +1,14 @@
 #import ctypes         #g++ -fPIC -shared -o Data_read.so data_read.cpp
 #Open_file = ctypes.CDLL("D:/Repositorios/Fundamentos_de_IA/P1/Data_read.so")#matrix read in c++ with headers                                                         #to calculate the size
+
+class Coord():
+    def __init__(self,Valor,Xcoordinate,Ycoordinate,visited_flag):#Para cada casilla se podrá tener este objeto
+        self.Valor=Valor                                           #que define su posición y si ya ha sido visitad
+        self.Xcoordinate=Xcoordinate                               #Al igual que el valor de la casilla
+        self.Ycoordinate=Ycoordinate
+        self.visited_flag=visited_flag     #Probablemente tengamos que añadir si se toma una desicion
+
+
 Matrix_rows=list()
 Matrix_rows1=list()
 with open("D:\Repositorios\Fundamentos_de_IA\P1\matriz.txt","r") as Read_matrix:
@@ -17,5 +26,18 @@ for linea in Matrix_rows:
 for line in Matrix_rows1:
     print(line)
 
+puntos=list()
+
+
+for indice,cadena in enumerate(Matrix_rows1):      #Vamos a agregar los puntos y su valor
+    for indice2,letra in enumerate(cadena):             #especificando todos los puntos como no visitados
+        puntos.append(Coord(letra,indice2,indice,False))
+        
+
+
+
+
 Coo1=input("Ingrese coordenada de inicio\n")
 Coo2=input("Ingrese coordenada de fin\n")
+
+
