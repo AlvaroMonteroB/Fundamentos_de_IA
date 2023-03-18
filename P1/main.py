21bf274fddcaea2ffa054778c1e23bd878943b68
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-17 21:58:01 -0600

    Main funct

--- P1/main.py
+++ P1/main.py
@@ -2,13 +2,13 @@
 #Open_file = ctypes.CDLL("D:/Repositorios/Fundamentos_de_IA/P1/Data_read.so")#matrix read in c++ with headers                                                         #to calculate the size
 Matrix_rows=list()
 Matrix_rows1=list()
-with open("D:\Repositorios\Fundamentos_de_IA\P1\matriz.txt") as Read_matrix:
+with open("D:\Repositorios\Fundamentos_de_IA\P1\matriz.txt","r") as Read_matrix:
     leer=Read_matrix.readline
-    while(Read_matrix.readline):
-        Matrix_rows.append(leer)
-        leer=Read_matrix.readline
+    for line in Read_matrix:
+        Matrix_rows.append(line)
     Read_matrix.close
-                                    #Lectura y remplazo de comas en el archivo
+                                    #Lectura y remplazo de comas en el archiv
+print("Read successfully")
 for linea in Matrix_rows:
     aux=linea.replace(',','')
     aux.strip()
@@ -17,3 +17,5 @@ for linea in Matrix_rows:
 for line in Matrix_rows1:
     print(line)
 
+Coo1=input("Ingrese coordenada de inicio\n")
+Coo2=input("Ingrese coordenada de fin\n")
