2d6dd4af1629bdb40308e25251cb13d3d0ef96c7
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-17 21:48:27 -0600

    change

--- P1/main.py
+++ P1/main.py
@@ -2,7 +2,7 @@
 #Open_file = ctypes.CDLL("D:/Repositorios/Fundamentos_de_IA/P1/Data_read.so")#matrix read in c++ with headers                                                         #to calculate the size
 Matrix_rows=list()
 Matrix_rows1=list()
-with open("matriz.txt") as Read_matrix:
+with open("D:\Repositorios\Fundamentos_de_IA\P1\matriz.txt") as Read_matrix:
     leer=Read_matrix.readline
     while(Read_matrix.readline):
         Matrix_rows.append(leer)
@@ -13,4 +13,7 @@ for linea in Matrix_rows:
     aux=linea.replace(',','')
     aux.strip()
     Matrix_rows1.append(aux)#Arreglo de renglones sin comas y sin saltos de linea
+
+for line in Matrix_rows1:
+    print(line)
 
