7530819202d457b6c3233d60e651debf15e80d74
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-17 21:38:23 -0600

    Read file from txt python

--- Agente1/BusqAnch.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-typedef struct Nodo{
-    Nodo *link;
-    char pos[2];
-}Nodo;
-
-
--- P1/main.py
+++ P1/main.py
@@ -1,4 +1,16 @@
-import ctypes         #g++ -fPIC -shared -o Data_read.so data_read.cpp
-Open_file = ctypes.CDLL("D:/Repositorios/Fundamentos_de_IA/P1/Data_read.so")#matrix read in c++ with headers                                                         #to calculate the size
+#import ctypes         #g++ -fPIC -shared -o Data_read.so data_read.cpp
+#Open_file = ctypes.CDLL("D:/Repositorios/Fundamentos_de_IA/P1/Data_read.so")#matrix read in c++ with headers                                                         #to calculate the size
+Matrix_rows=list()
+Matrix_rows1=list()
+with open("matriz.txt") as Read_matrix:
+    leer=Read_matrix.readline
+    while(Read_matrix.readline):
+        Matrix_rows.append(leer)
+        leer=Read_matrix.readline
+    Read_matrix.close
+                                    #Lectura y remplazo de comas en el archivo
+for linea in Matrix_rows:
+    aux=linea.replace(',','')
+    aux.strip()
+    Matrix_rows1.append(aux)#Arreglo de renglones sin comas y sin saltos de linea
 
-Matrix=Open_file.matrix("matriz.txt")
