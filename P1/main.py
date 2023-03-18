6e2c200f560815bff1187a55a17e383e93e7652b
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-17 20:54:26 -0600

    Matrix open

--- P1/main.py
+++ P1/main.py
@@ -1,4 +1,4 @@
 import ctypes         #g++ -fPIC -shared -o Data_read.so data_read.cpp
 Open_file = ctypes.CDLL("D:/Repositorios/Fundamentos_de_IA/P1/Data_read.so")#matrix read in c++ with headers                                                         #to calculate the size
 
-
+Matrix=Open_file.matrix("matriz.txt")
