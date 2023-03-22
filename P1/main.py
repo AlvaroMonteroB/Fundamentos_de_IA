7a0eeff76e52f1abb799d048e9ef216dd62304b4
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-21 22:17:45 -0600

    creando menú

--- P1/main.py
+++ P1/main.py
@@ -1,7 +1,8 @@
 import threading
 import display_graphics
 import Read_data
-
+import Criaturas
+import Agentes
 
 C_ini=list()
 C_fin=list()
@@ -18,9 +19,21 @@ C_fin.append(int(Coo2[0]))
 C_fin.append(int(Coo2[1]))#Puntos con sus respectivas coordenadas
 
 
-
-
-def access_obj(cols,rows,Mat_obj):
-    line=Mat_obj[rows-1]
-    return line[cols-1]
-
+entity=Criaturas.character()
+opt=input("Choose a character:\n1.-Human\n2.-Monkey\n3.-Octopus\n4.-Sasquatch\n")
+pepito=input("Choose a name")
+if opt==1:
+    entity=Criaturas.character.Humano(pepito)
+elif opt==2:
+    entity=Criaturas.character.Monkey(pepito)
+elif opt==3:
+    entity=Criaturas.character.Octopus(pepito)
+elif opt==4:
+    entity=Criaturas.character.Sasquatch(pepito)
+else:
+    print("Put a valid option")
+    
+    
+opt=input("Choose an Agent:\n\n2\n3\n4\n5\n")
+if opt==1:
+    agent=Agentes.agente1(1,C_ini,entity)
