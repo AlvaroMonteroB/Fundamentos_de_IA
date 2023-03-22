2981dab7ed2024856862c17a09cdb211019138bd
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-21 22:18:13 -0600

    Impresion de puntos

--- P1/Read_data.py
+++ P1/Read_data.py
@@ -1,12 +1,18 @@
+# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
+data_terrain={0:"Montaña", 1:"Tierra", 2:"Agua",3:"Arena",4:"Bosque",5:"Pantano"}
 
 
 class Coord():
     
-    def __init__(self,Valor,Xcoordinate,Ycoordinate,visited_flag):#Para cada casilla se podrá tener este objeto
+    def __init__(self,Valor,Xcoordinate,Ycoordinate,visited_flag,seen_flag):#Para cada casilla se podrá tener este objeto
         self.Valor=Valor                                           #que define su posición y si ya ha sido visitad
         self.Xcoordinate=Xcoordinate                               #Al igual que el valor de la casilla
         self.Ycoordinate=Ycoordinate
         self.visited_flag=visited_flag     #Probablemente tengamos que añadir si se toma una desicion
+        self.seen_flag=seen_flag
+    def print_data(self,cost):
+        print("Data of point "+self.Xcoordinate+","+self.Ycoordinate)
+        print("El terreno es "+data_terrain[self.Valor]+" Tiene un costo de "+cost+" Esta casilla")
 
 def read_matrix():
     Matrix_rows=list()
@@ -31,12 +37,12 @@ def read_matrix():
 
     for indice,cadena in enumerate(Matrix_rows1):      #Vamos a agregar los puntos y su valor
         for indice2,letra in enumerate(cadena):             #especificando todos los puntos como no visitados
-            puntos.append(Coord(letra,indice2,indice,False))
-    output=Mat_and_obj(Matrix_rows1, puntos)
-    return output
+            puntos.append(Coord(letra,indice2,indice,False,False))
+    return puntos
 
 
-class Mat_and_obj:
-    def __init__(self,matriz, arreglo)->None:
-        self.matriz=matriz
-        self.arreglo=arreglo
+        
+def access_obj(cols,rows,Mat_obj:Coord):#Hay que modificar este
+    line=Mat_obj[rows-1]
+    return line[cols-1]
+
