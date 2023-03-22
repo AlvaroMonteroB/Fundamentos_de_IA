5a227b0b786b937e47373c18e9869a8515f5c51d
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-22 15:45:15 -0600

    point structure

--- P1/main.py
+++ P1/main.py
@@ -2,23 +2,27 @@ import threading
 import display_graphics
 import Read_data
 import Criaturas
+import various_methods
 import Agentes
-
+C_aux=list()
 C_ini=list()
 C_fin=list()
 Obj_comp=Read_data.read_matrix()#arreglo de puntos AKA objetos
-Matrix=Obj_comp.matriz()
-puntos=Obj_comp.arreglo()
 Coo1=input("Ingrese coordenada de inicio\n")
 Coo2=input("Ingrese coordenada de fin\n")
-Coo1.replace(',','')
-Coo2.replace(',','')
-C_ini.append(int(Coo1[0]))
-C_ini.append(int(Coo1[1]))
-C_fin.append(int(Coo2[0]))
-C_fin.append(int(Coo2[1]))#Puntos con sus respectivas coordenadas
+for letra in Coo1:
+    if not letra==',' :
+        C_aux.append(letra)
+    C_ini.append(int(C_aux))
+    C_aux.clear()
+for letra in Coo2:
+    if not letra==',':
+        C_aux.append(letra)
+    C_fin.append(int(C_aux))
+    C_aux.clear()
+#Puntos con sus respectivas coordenadas
 
-
+various_methods.busq_point(Obj_comp, C_ini[0], C_ini[1])
 entity=Criaturas.character()
 opt=input("Choose a character:\n1.-Human\n2.-Monkey\n3.-Octopus\n4.-Sasquatch\n")
 pepito=input("Choose a name")
@@ -33,7 +37,9 @@ elif opt==4:
 else:
     print("Put a valid option")
     
-    
-opt=input("Choose an Agent:\n\n2\n3\n4\n5\n")
+
+opt=input("Choose an Agent:\n1\n2\n3\n4\n5\n")
 if opt==1:
     agent=Agentes.agente1(1,C_ini,entity)
+elif opt==2:
+    agent=Agentes.Agente3()
