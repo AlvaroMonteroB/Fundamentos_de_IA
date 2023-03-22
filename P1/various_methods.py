f3432e8669045e329fa1392bdf6827b22f4485fe
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-22 15:45:57 -0600

    finding and assign

--- P1/various_methods.py
+++ P1/various_methods.py
@@ -7,9 +7,22 @@ def terrain_info(punto,ente):
     punto.print_data(ente.cost)
     return 
 
-def busq_point(Matrix:Read_data.Coord,X:int,Y:int)-> Read_data.Coord(Valor, Xcoordinate, Ycoordinate, visited_flag, seen_flag):#busca el punto entre nuestra lista de objetos para devolverlo
-    i=0
-    for obj in Matrix:
+def busq_point(Matrix:Read_data.Coord,X:int,Y:int)-> Read_data.Coord:#busca el punto entre nuestra lista de objetos para devolverlo
+    for obj in Matrix:#iteramos objeto en la matriz
         if obj.Ycoordinate==Y&obj.Xcoordinate==X:
             output=obj
-    return obj
+    if output:#Si encontró punto, la variable output va a existir
+        output.seen_flag=True
+        return output
+    elif not output:#En caso de no estar dentro del mapa, se lanza un punto no válido
+        output=Read_data.Coord('Not valid', -1, -1, False, false)
+        return output
+    
+
+def assign_point(Matrix:Read_data.Coord,X:int,Y:int):
+    for obj in Matrix:#iteramos objeto en la matriz
+        if obj.Ycoordinate==Y&obj.Xcoordinate==X:
+            output=obj
+    output.visited_flag=True
+    output.seen_flag=True
+    return output
