f9669b1212e70b18ce573abfa9397d48d106d153
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-21 22:33:46 -0600

    Subrutina de escaneo

--- P1/Agentes.py
+++ P1/Agentes.py
@@ -1,10 +1,12 @@
 import Read_data
 import Criaturas
+import various_methods
 class agente1:
-    def __init__(self,direction,position:Read_data.Coord,charact:Criaturas.character) -> None:
+    def __init__(self,direction,position:Read_data.Coord,charact:Criaturas.character,Matrix:Read_data.Coord) -> None:
         self.direction=direction
         self.position=position
         self.charact=charact
+        self.Matrix=Matrix
     def turn_left(self):#dirs: 1=-> 2=^ 3=<- 4=v
         if self.direction<3&self.direction>0:
             self.direction+1
@@ -12,7 +14,10 @@ class agente1:
             self.direction=1
     def scan_forward(self):#Censado
         if self.direction==1:
-            scan_pos=self.position
+            X=self.position.Xcoordinate+1
+            scanned_pos=various_methods.busq_point(self.Matrix,X,self.position.Ycoordinate)
+            self.Matrix.seen_flag=True
+            
             
             
             
--- P1/various_methods.py
+++ P1/various_methods.py
@@ -6,3 +6,10 @@ import Criaturas
 def terrain_info(punto,ente):
     punto.print_data(ente.cost)
     return 
+
+def busq_point(Matrix:Read_data.Coord,X:int,Y:int):
+    i=0
+    for obj in Matrix:
+        if obj.Ycoordinate==Y&obj.Xcoordinate==X:
+            output=obj
+    return obj
