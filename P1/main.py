33efb1feb4141e2756a4df28bca52ad09c0c4522
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-23 14:12:19 -0600

    IF OPTN char

--- P1/main.py
+++ P1/main.py
@@ -12,58 +12,65 @@ Matrix=list()
 Read_data.read_matrix(Matrix)#arreglo de puntos AKA objetos
 Coo1=input("Ingrese coordenada de inicio\n")
 Coo2=input("Ingrese coordenada de fin\n")
+aux=''
 for letra in Coo1:
-    if not letra==',' :
+    if not letra==',' :#Si no es ',' pasa al siguiente paso
         C_aux.append(letra)
-    C_ini.append(C_aux.data())
+    for val in C_aux:
+        aux=aux+val
+    C_ini.append(int(aux))
     C_aux.clear()
+aux=''
 for letra in Coo2:
     if not letra==',':
         C_aux.append(letra)
-    C_fin.append(C_aux.data())
+    for val in C_aux:
+        aux=aux+val
+    C_fin.append(int(aux))
     C_aux.clear()
+aux=''
 #Puntos con sus respectivas coordenadas
 
 various_methods.busq_point(Matrix, C_ini[0], C_ini[1])
 entity=Criaturas.character()
+pepito=input("Choose a name\n")
 opt=input("Choose a character:\n1.-Human\n2.-Monkey\n3.-Octopus\n4.-Sasquatch\n")
-pepito=input("Choose a name")
-if opt==1:
+if opt=='1':
     entity=Criaturas.character.Humano(pepito)
-elif opt==2:
+elif opt=='2':
     entity=Criaturas.character.Monkey(pepito)
-elif opt==3:
+elif opt=='3':
     entity=Criaturas.character.Octopus(pepito)
-elif opt==4:
+elif opt=='4':
     entity=Criaturas.character.Sasquatch(pepito)
 else:
-    print("Put a valid option")
+    print("Put a valid option\n")
     
-user=input("Do you ant to play against the machine?  y/n")
+user=input("Do you ant to play against the machine?  y/n\n")
 if user=='y':
     userband=False #not the user
 else:
      userband=True
 
 opt=input("Choose an Agent:\n1\n2\n3\n4\n5\n")
-if opt==1:
+if opt=='1':
     if userband:
         agent=Agentes.agente1(1,C_ini,entity,Matrix,userband)#true=PC, false=User
         agentA=Agentes.agente1(1,C_ini,entity,Matrix,True)
     else:
         agentA=Agentes.agente1(1,C_ini,entity,Matrix,True)
-elif opt==2:
+elif opt=='2':
     if userband:
         agent=Agentes.Agente2(1,C_ini,entity,Matrix,userband)#true=PC, false=User
         agentA=Agentes.Agente2(1,C_ini,entity,Matrix,True)
     else:
         agentA=Agentes.Agente2(1,C_ini,entity,Matrix,True)
-elif opt==3:
+elif opt=='3':
     agent=Agentes.Agente3()
 
-Raiz=list()
+Raiz=list()#Iniciando la raiz
 if userband: #true if user doesnt do anything
-    Raiz.append(agentA.position)
+    Raiz.append(agentA.position)#aqui se añade el elemento a la raiz
 else:
     Raiz.append(agent.position)
     Raiz2=list()
