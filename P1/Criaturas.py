686a9e6296e567e5913e7146248d58e19f7a206c
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-03-28 17:04:53 -0600

    solving, int to string

--- P1/Criaturas.py
+++ P1/Criaturas.py
@@ -1,61 +1,61 @@
 
 def humano(casilla):
-    if casilla==0:# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
+    if casilla=='0':# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
                 return 0
-    if casilla==1:
+    if casilla=='1':
                 return 2
-    if casilla==2:
+    if casilla=='2':
                 return 2
-    if casilla==3:
+    if casilla=='3':
                 return 3
-    if casilla==4:#bosque
+    if casilla=='4':#bosque
                 return 4
-    if casilla==5:
+    if casilla=='5':
                 return 5
     else:
                 return -1
 def Sasquatch(casilla):
-            if casilla==0:# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
+            if casilla=='0':# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
                 return 15
-            if casilla==1:
+            if casilla=='1':
                 return 4
-            if casilla==2:
+            if casilla=='2':
                 return 0
-            if casilla==3:
+            if casilla=='3':
                 return 0
-            if casilla==4:#bosque
+            if casilla=='4':#bosque
                 return 4
-            if casilla==5:
+            if casilla=='5':
                 return 5
             else:
                 return -1
 def Monkey(casilla):
-            if casilla==0:# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
+            if casilla=='0':# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
                 return 0
-            if casilla==1:
+            if casilla=='1':
                 return 2
-            if casilla==2:
+            if casilla=='2':
                 return 4
-            if casilla==3:
+            if casilla=='3':
                 return 3
-            if casilla==4:#bosque
+            if casilla=='4':#bosque
                 return 1
-            if casilla==5:
+            if casilla=='5':
                 return 5
             else:
                 return -1
 def Octopus(casilla):
-            if casilla==0:# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
+            if casilla=='0':# 0=montaña, 1 tierra, 2 agua, 3 arena, 4 bosque, 5 pantano
                 return 0
-            if casilla==1:
+            if casilla=='1':
                 return 2
-            if casilla==2:
+            if casilla=='2':
                 return 1
-            if casilla==3:
+            if casilla=='3':
                 return 0
-            if casilla==4:#bosque
+            if casilla=='4':#bosque
                 return 3
-            if casilla==5:
+            if casilla=='5':
                 return 2
             else:
                 return -1
