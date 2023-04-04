dd205f1bf98aa9cfc7f13b873caa2bd6da5ef9e2
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-04-04 10:09:43 -0600

    init queue

--- P1/Busq_Anch.py
+++ P1/Busq_Anch.py
@@ -3,6 +3,20 @@ import Busq_prof as b_p
 class Queue:
     def __init__(self) -> None:
         self.items=[]
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
 
 def alg_busq1():
     cola=Queue()
