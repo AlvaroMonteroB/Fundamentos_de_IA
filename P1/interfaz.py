37e500624168d47e72cd8b3f2ab01c9b99a85df1
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-04-08 00:47:38 -0600

    Camino recorrido

--- P1/interfaz.py
+++ P1/interfaz.py
@@ -4,7 +4,7 @@ import Read_data as rd
 
 
 
-def interfaz(Matrix:rd.Coord):
+def interfaz(Matrix:rd.Coord,jugador:bool):
     list = Matrix
     a = len(list)
     length = 500//a
@@ -21,22 +21,40 @@ def interfaz(Matrix:rd.Coord):
         y = i * length
         for j in range(a):
             x = j * length
-            if list[i][j].Valor == '0': #montaña
-                color = "#57605A"
-            elif list[i][j].Valor == '1': #tierra
-                color = "#5D370F"
-            elif list[i][j].Valor == '2': #agua
-                color = "#06ACEE"
-            elif list[i][j].Valor == '3': #arena
-                color = "#E2C58B"
-            elif list[i][j].Valor == '4': #bosque
-                color = "#00C606"
-            elif list[i][j].Valor == '5': #pantano
-                color = "#830EC6"
-            elif list[i][j].Valor == '6': #valor para ver si no ha sido visitado
-                color = "#000000"
-            else:
-                color = "FFFFFF"
+            terrain=list[i][j]
+            if not terrain.seen_flag:#Si no lo hemos visto lo pasamos a negro
+                color="#000000"
+            elif terrain.visited_flag:
+                if terrain.Valor == '0': #montaña
+                    color = "#57605A"
+                elif terrain.Valor == '1': #tierra
+                    color = "#5D370F"
+                elif terrain.Valor == '2': #agua
+                    color = "#06ACEE"
+                elif terrain.Valor == '3': #arena
+                    color = "#E2C58B"
+                elif terrain.Valor == '4': #bosque
+                    color = "#00C606"
+                elif terrain.Valor == '5': #pantano
+                    color = "#830EC6"
+                else:
+                    color = "FFFFFF"
+            elif terrain.seen_flag and not terrain.visited_flag:
+                if terrain.Valor == '0': #montaña
+                    color = "#7D7D7D"
+                elif terrain.Valor == '1': #tierra
+                    color = "#7D5F3F"
+                elif terrain.Valor == '2': #agua
+                    color = "#6DADBB"
+                elif terrain.Valor == '3': #arena
+                    color = "#B8A972"
+                elif terrain.Valor == '4': #bosque
+                    color = "#007F00"
+                elif terrain.Valor == '5': #pantano
+                    color = "#7D3F7D"
+
+                else:
+                    color = "FFFFFF"
             canvas.create_rectangle(x, y, x+length, y+length, fill=color)
 
     f = Frame(ventana, bg="#FDF6FF")
@@ -56,3 +74,5 @@ def interfaz(Matrix:rd.Coord):
 
 
     ventana.mainloop()
+
+
