eeff0924e930a9c939e1003721319668c454f27e
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-04-25 18:07:18 -0600

    main

---
+++ ProyectoP1/main.py
@@ -0,0 +1,14 @@
+import A_estrella as ae
+import Agentes as ag
+
+
+
+
+
+
+
+
+puntos=list()
+puntos.append(None)
+for punto in range(1,len(puntos)):
+    ae.Init_busq(raiz, Agente, Matrix, fin_pos)
