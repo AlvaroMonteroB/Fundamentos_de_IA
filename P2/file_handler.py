7a77352370603e25333c5b5317e895f77e971ac7
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-05-16 17:36:44 -0600

    file handler

--- P2/file_handler.py
+++ P2/file_handler.py
@@ -1,14 +1,29 @@
 import csv
+class file_handler:
+    def __init__(self,path):
+        with open(path,'r') as f:
+            self.archivo=csv.reader(f)
+            f.close()
+        self.campos=len(next(self.archivo))#Numero de columnas
+        self.pattern=len(list(self.archivo))#numero de filas
+
+    def set_fields(self,type_list):#Una lista que guarda que tipo va a ser cada campo
+            if len(type_list)==self.campos:
+                self.type_list=type_list
+    
+
+    def get_cell(self, iterador, posicion:int):
+        if iterador<self.pattern and iterador>=0 and posicion<self.campos and posicion>=0:
+            return switch_type[self.type_list[posicion]](self.archivo[iterador])
+        else:
+            return None
 
 
-def read_file(path):
-    with open(path,'r') as f:
-        lectura=csv.reader(f)
-        f.close()
-    return lectura
 
-def write_file(path,data):
-    with open(path,'w') as f:
-        f.write(data)
-        f.close()
+switch_type={
+    1:int,
+    2:float,
+    3:str
+}
+
 
