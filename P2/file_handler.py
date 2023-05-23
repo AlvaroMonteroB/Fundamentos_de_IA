88621535003dc47369026da2bcc595d70c10b8b8
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-05-23 16:27:16 -0600

    new methods

--- P2/file_handler.py
+++ P2/file_handler.py
@@ -14,14 +14,24 @@ class file_handler:
             else: 
                 return False
 
+
     def auto_set(self):
         types=list()
         aux=self.archivo[0]
-        for column in aux:
+        for column,i in aux,range(len(aux)):
             if '.' in column:
                 types.append(2)
             elif column.isdigit():
-                types.append(1)
+                count=0
+                for col in self.archivo:#Vamos a determinar si es bool
+                    if col[i]!=0 and  col[i]!=1:
+                        count=+1
+                        if count>0:
+                            break
+                if count==0:
+                    types.append(4)
+                else:
+                    types.append(1)
             else:
                 types.append(3)
         self.campos=types
@@ -32,30 +42,41 @@ class file_handler:
         else:
             print("class not set")
 
-    def detect_different_classes(self):
+    def detect_different_classes(self):#Detectar cuantos tipos de diferentes clases hay
         self.type_class=list()
-        for row in self.archivo:
-            if not (row[self.clase] in self.clases) :
-               self.type_class.append(row[self.clase])
+        for row in self.archivo:#columnas en archivo
+            if not (row[self.clase] in self.clases):#si row[iterador de clase] no está en clases(lista de las diferentes clases)
+               self.type_class.append(row[self.clase])#Si no está, agregamos la siguiente clase
             else:
                 continue
 
             
 
-    def get_cell(self, iterador, posicion:int):
+    def get_cell(self, iterador, posicion:int):#Hacer un select directo a una celda
         if iterador<self.pattern and iterador>=0 and posicion<self.campos and posicion>=0 and self.type_list!=None:
             return switch_type[self.type_list[posicion]](self.archivo[iterador])
         else:
             return None
     
-    def get_row(self,iterador):
+    def get_row(self,iterador):#Devuelve el renglon pero con los cast adecuados
         if iterador<self.pattern and iterador>=0 and self.type_list!=None:
             aux= self.archivo[iterador]
             row=list()
-            for i in range(aux):
+            for i in range(self.pattern):
                 row.append(switch_type[self.type_list[posicion]](aux[i]))                
         else:
             return None
+    
+    def get_rows(self,ini=0,fin=self.pattern):
+        if ini<fin and ini<self.pattern-1 and ini>=0 and self.typelist!=None and fin<=self.pattern:
+            rows=list()
+            for it in range(ini,fin,1):
+                aux=self.archivo[it]
+                row=list()
+                for i in range(self.pattern):
+                    row.append(switch_type[self.type_list[posicion]](aux[i]))
+                rows.append(row)
+            return rows
 
 
 
@@ -63,6 +84,7 @@ switch_type={
     1:int,
     2:float,
     3:str,
+    4:bool
 }
 
 
--- P2/main.py
+++ P2/main.py
@@ -9,14 +9,18 @@ path=input("Introduce el path de tu archivo\n")
 dataset=fh.file_handler(path)
 condition=True
 fields=list()
-while condition:
-    print(dataset.archivo[0])
-    for i in range(dataset.campos):
-        aux=input("Introduce campo "+str(i+1)+" ||  int=1, float=2, str=3\n")
-        fields.append(int(aux))
-    cls()
-    if dataset.set_fields(fields):
-        condition=False
-    else:
-        print("Intrduce campos validos\n")
+a=input("Quieres hacer auto detect de los campos?  1.-Si    0.-No")
+if not a:
+    while condition:
+        print(dataset.archivo[0])
+        for i in range(dataset.campos):
+            aux=input("Introduce campo "+str(i+1)+" ||  int=1, float=2, str=3\n")
+            fields.append(int(aux))
+        cls()
+        if dataset.set_fields(fields):
+            condition=False
+        else:
+            print("Intrduce campos validos\n")
+else:
+    dataset.auto_set()
 
